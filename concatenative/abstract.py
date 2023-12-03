4cc3acb9acb07c5858fe3dbbdc56ce72015d3f04
daddinuz <daddinuz@gmail.com>
2023-12-03 23:43:04 +0100

    extract Apply interface from Concatentive, removed concat method

--- concatenative/abstract.py
+++ concatenative/abstract.py
@@ -2,28 +2,26 @@ from typing import final, override
 from abc import ABC, abstractmethod
 
 
-class Concatenative[I, O](ABC):
+class Apply[I, O](ABC):
     @abstractmethod
     def apply(self, value: I) -> O:
         pass
 
+
+class Concatenative[I, O](Apply[I, O]):
     @final
     def __call__(self, value: I) -> O:
         return self.apply(value)
 
     @final
-    def concat[K](self, other: "Concatenative[O, K]") -> "Concatenative[I, K]":
-        class Concatenated(Concatenative[I, K]):
-            def __init__(self, head: Concatenative[I, O], tail: Concatenative[O, K]):
+    def __or__[Q](self, other: "Concatenative[O, Q]") -> "Concatenative[I, Q]":
+        class Concatenated(Concatenative[I, Q]):
+            def __init__(self, head: Apply[I, O], tail: Apply[O, Q]):
                 self._head = head
                 self._tail = tail
 
             @override
-            def apply(self, value: I) -> K:
+            def apply(self, value: I) -> Q:
                 return self._tail.apply(self._head.apply(value))
 
         return Concatenated(self, other)
-
-    @final
-    def __or__[K](self, other: "Concatenative[O, K]") -> "Concatenative[I, K]":
-        return self.concat(other)
